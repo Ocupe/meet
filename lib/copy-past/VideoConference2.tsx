b90b362c69722a359b00de7e6334ccff4a2c52ea
Jonas Schell <jonas@livekit.io>
2023-10-09 15:47:15 +0800

    add ParticipantTileWithReaction

--- lib/copy-past/VideoConference2.tsx
+++ lib/copy-past/VideoConference2.tsx
@@ -22,6 +22,7 @@ import { useCreateLayoutContext } from '@livekit/components-react';
 import { usePinnedTracks, useTracks } from '@livekit/components-react';
 import { Chat } from '@livekit/components-react';
 import { ControlBar2 } from '../copy-past/ControlBar2';
+import { ParticipantTileWithReaction } from './ParticipantTileWithReaction';
 
 /**
  * @public
@@ -124,7 +125,7 @@ export function VideoConference2({
             {!focusTrack ? (
               <div className="lk-grid-layout-wrapper">
                 <GridLayout tracks={tracks}>
-                  <ParticipantTile />
+                  <ParticipantTileWithReaction />
                 </GridLayout>
               </div>
             ) : (
